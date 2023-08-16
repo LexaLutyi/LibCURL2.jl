55901478aa35aaa38ec25cb9f0a5ca62f88f7829
Documenter.jl <documenter@juliadocs.github.io>
2023-08-16 10:59:02 +0000

    build based on 3661ae7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LibCURL2","category":"page"},{"location":"#LibCURL2","page":"Home","title":"LibCURL2","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LibCURL2.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LibCURL2]","category":"page"}]
+}
