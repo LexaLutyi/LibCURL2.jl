161243992638009a6e2bfb6e6a5086e724caeb18
Samarin Aleksei <liotbiu1@gmail.com>
2023-08-16 16:15:32 +0400

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using LibCURL2
 using Test
 
 @testset "LibCURL2.jl" begin
-    # Write your tests here.
+    curl = curl_easy_init()
+    @show curl_easy_setopt(curl, CURLOPT_URL, "https://example.com")
+    res = curl_easy_perform(curl)
+    if res != CURLE_OK
+        curl_error = unsafe_string(curl_easy_strerror(res))
+        error(curl_error)
+    end
+    curl_easy_cleanup(curl)
 end
