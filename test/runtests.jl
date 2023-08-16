68353c949ab8905c0d236de3333c3f6c033d5e17
Samarin Aleksei <liotbiu1@gmail.com>
2023-08-16 17:36:37 +0400

    remove CURL_EXTERN

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,41 @@
 using LibCURL2
 using Test
 
+
+# Setup the callback function to recv data
+function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, n::Csize_t, p_ctxt::Ptr{Cvoid})
+    sz = s * n
+    data = Array{UInt8}(undef, sz)
+    ccall(:memcpy, Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Cvoid}, Csize_t), data, curlbuf, sz)
+    return sz::Csize_t
+end
+
+
 @testset "LibCURL2.jl" begin
     curl = curl_easy_init()
-    @show curl_easy_setopt(curl, CURLOPT_URL, "https://example.com")
-    res = curl_easy_perform(curl)
-    if res != CURLE_OK
-        curl_error = unsafe_string(curl_easy_strerror(res))
-        error(curl_error)
+    curl == C_NULL && error("curl_easy_init() failed")
+    
+
+    # Set up the write function to consume the curl output so we don't see it in the
+    # test output
+    c_curl_write_cb = @cfunction(
+        curl_write_cb,
+        Csize_t,
+        (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})
+    )
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, c_curl_write_cb)
+
+    # Set up our SSL options
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com")
+    curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL)
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2)
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1)
+    curl_easy_setopt(curl, CURLOPT_CAINFO, LibCURL2.cacert)
+
+    @testset "SSL Success" begin
+        res = curl_easy_perform(curl)
+        @test res == CURLE_OK
     end
+    
     curl_easy_cleanup(curl)
 end
