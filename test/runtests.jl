5a4c5e27c1e1e18464cc308c4aa8210e23ba79cd
Samarin Aleksei <liotbiu1@gmail.com>
2023-08-30 04:46:11 +0400

    test macos

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,68 +3,56 @@ using LibCURL2_jll
 using Test
 
 
-@testset "Local LibCURL2_jll" begin
-    # const CURLOPT_URL = 10002 % UInt32
-    # const CURLcode = UInt32
-    # const CURLE_OK = 0 % UInt32
-
-    curl = @ccall libcurl.curl_easy_init()::Ptr{Cvoid}
-
-    @ccall libcurl.curl_easy_setopt(curl::Ptr{Cvoid}, CURLOPT_URL::UInt64, "https://www.google.com"::Cstring)::CURLcode
-
-    req = @ccall libcurl.curl_easy_perform(curl::Ptr{Cvoid})::CURLcode
-
-    @show req
-    err = @ccall libcurl.curl_easy_strerror(req::CURLcode)::Ptr{UInt8}
-    err |> unsafe_string |> println
-end
-
-
-@testset "Minimal example" begin
-    curl = curl_easy_init()
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com")
-    req = curl_easy_perform(curl)
-    @show req
-    curl_easy_cleanup(curl)
-end
-
-
 # Setup the callback function to recv data
 function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, n::Csize_t, p_ctxt::Ptr{Cvoid})
     sz = s * n
-    data = Array{UInt8}(undef, sz)
-    ccall(:memcpy, Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Cvoid}, Csize_t), data, curlbuf, sz)
     return sz::Csize_t
 end
 
+c_curl_write_cb = @cfunction(
+    curl_write_cb,
+    Csize_t,
+    (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})
+)
 
-@testset "LibCURL2.jl" begin
+
+@testset "LibCURL2_jll" begin
+    curl = @ccall libcurl.curl_easy_init()::Ptr{Cvoid}
+    # set url
+    @ccall libcurl.curl_easy_setopt(curl::Ptr{Cvoid}, CURLOPT_URL::UInt64, "https://www.google.com"::Cstring)::CURLcode
+    # set callback
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, c_curl_write_cb)
+    # set ssl
+    @ccall libcurl.curl_easy_setopt(curl::Ptr{Cvoid}, CURLOPT_USE_SSL::UInt64, CURLUSESSL_ALL::UInt64)::CURLcode
+    @ccall libcurl.curl_easy_setopt(curl::Ptr{Cvoid}, CURLOPT_SSL_VERIFYHOST::UInt64, 2::UInt64)::CURLcode
+    @ccall libcurl.curl_easy_setopt(curl::Ptr{Cvoid}, CURLOPT_SSL_VERIFYPEER::UInt64, 1::UInt64)::CURLcode
+    @ccall libcurl.curl_easy_setopt(curl::Ptr{Cvoid}, CURLOPT_CAINFO::UInt64, LibCURL2.cacert::Cstring)::CURLcode
+    # perform
+    code = @ccall libcurl.curl_easy_perform(curl::Ptr{Cvoid})::CURLcode
+    # code as str
+    str_ptr = @ccall libcurl.curl_easy_strerror(code::CURLcode)::Ptr{UInt8}
+    str = unsafe_string(str_ptr)
+
+    @test code == CURLE_OK
+    @test str == "No error"
+end
+
+
+@testset "LibCURL2" begin
     curl = curl_easy_init()
     curl == C_NULL && error("curl_easy_init() failed")
     
-
-#     # Set up the write function to consume the curl output so we don't see it in the
-#     # test output
-    # c_curl_write_cb = @cfunction(
-    #     curl_write_cb,
-    #     Csize_t,
-    #     (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})
-    # )
-    # curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, c_curl_write_cb)
-
-#     # Set up our SSL options
     curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com")
     curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL)
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2)
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1)
     curl_easy_setopt(curl, CURLOPT_CAINFO, LibCURL2.cacert)
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, c_curl_write_cb)
 
-    @testset "SSL Success" begin
-        res = curl_easy_perform(curl)
-        @test res == CURLE_OK
-        # curl_error = unsafe_string(curl_easy_strerror(rc))
-        # error(curl_error)
-    end
+    code = curl_easy_perform(curl)
+    str = unsafe_string(curl_easy_strerror(code))
+    @test code == CURLE_OK
+    @test str == "No error"
     curl_easy_cleanup(curl)
 end
 
